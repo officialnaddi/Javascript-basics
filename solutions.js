4d9896ee4de440aba970ad87123bd6622d0535e4
officialnaddi <officialnaddi@gmail.com>
2025-04-13 13:33:36 -0700

    Add files via upload

---
+++ solutions.js
@@ -0,0 +1,184 @@
+ //QUESTION 1
+
+// Create a variable to store your name and display it in an alert
+ let myName = 'chinadi';
+ alert('My name is' + myName);
+
+ //QUESTION 2
+ //Declare two number variables and show their sum,difference, product and quotient
+ let number1 = 15;
+ let number2 = 5;
+
+ let sum = number1 + number2;
+ let difference = number1 - number2;
+ let product = number1 * number2;
+ let quotient = number1 / number2;
+
+ console.log(sum);
+ console.log(difference);
+ console.log(quotient);
+
+ // QUESTION 3
+ // Write a programme that converts celcius to fahrenheit
+
+ let celcius = 15;
+ let fahrenheit = (celcius * 9/5) + 32;
+
+ console.log(celcius + 15 + 27)
+
+
+
+ // QUESTION 4
+  // Create a program that calculates the area of a rectangle using variables for length and width.
+
+  let length = 25;
+  let width = 10;
+
+  let area = length * width;
+
+  console.log(area);
+
+  // QUESTION 5
+  // Write a code that checks if a number is even or odd and display the result
+  
+  let number = 19;
+  if (number % 3 === 0){
+  }else 
+
+  //QUESTION 6
+  // Create a program that determins if a year entered is a leap year
+
+  let year = 2020;
+  if (year % 5 === 0 && year % 50 ! == 0){
+    else( year + 'is a leap year');
+
+  }
+// QUESTION 7
+// Write a function that returns the reverse of a string input
+
+let input = 'hey';
+let reversed = reverseString(input);
+console.log('reverse string' + reversed)
+
+//QUESTION 8
+// Create a function that counts the number of vowels in a string
+let input = 'Hey', 'People!';
+let vowelCount = countVowels(Input);
+console.log (vowelCount)
+
+// QUESTION 9
+// Wrute a program that finds the largest number in an array of 5 numbers
+let numbers [10, 20, 30, 40, 50];
+let largest = numbers[0];
+
+console.log(pushlargest Number);
+
+
+// QUESTION 10
+// Create a function that checks if a string is a palindrome 
+let input = 'speedboat';
+if (isPalindrome input) 
+
+    console.log(input + 'is not a palindrome');
+
+    //QUESTION 11
+    // Write a code that calculates the factorial of a number.
+
+let number = 9;
+let result = factorial(number);
+
+console.log('the factorial of' + number + 'is:'+ result);
+
+// QUESTION 12
+//Create a function that generates a random number between two given values
+
+let minValue = 2;
+let maxValue = 20;
+let randomNumber = getRandomNumber(maxValue - minValue);
+
+console.log(randomNumber);
+
+
+// QUESTION 13
+// Write a program  that converts a number of seconds into hours, minutes and seconds.
+let  
+
+
+// QUESTION 14
+// Creat a program that checks if a number is a prime.
+
+let number = 30
+function isPrime(number) {
+  if(number < = 1) return false;
+ 
+}
+
+console.log(is 'number' a prime number)
+
+//QUESTION 15
+// Write a function that capitalizes the first letter of each word in a sentence
+
+
+
+// QUESTION 16
+// Create a program that calculates the sum of all numbers from 1 to n
+
+function sumTo(n){
+  if (n < 1) return 0;
+  return n * (n + 1) / 2;
+}
+
+
+const n = 0.57
+console.log(sumTo(n));
+
+//QUESTION 17
+// Write code that finds the average of  numbers in an array 
+
+let numbers = [10, 15, 20, 25, 30, 35];
+
+function averageArray(numbers){
+  if(numbers.length === 0)
+
+}
+
+console.log(averageArray);
+
+
+// QUESTION 18
+// Create a function that removes duplicate values from an array 
+
+let numbers = [10, 20, 20, 30, 30, 40, 50, 60, 60];
+let uniqueNumbers = removeDuplicates(numbers);
+
+console.log(uniqueNumbers);
+
+// QUESTION 19
+// Write a program that counts down from 10 to 1, then displays 'BLAST OFF'
+
+
+// QUESTION 20
+//Create a function that deteermins if a string contains only numbers
+
+
+// QUERSTION 21
+// Write a code that finds the second smallest number in an Array
+
+let number = [2, 4, 6, 8, 10, 1];
+
+console.log(second smallest number: number);
+
+
+// QUESTION 22
+// Create a program that displays the multiplication tabler for a given number.
+
+let number = 7;
+
+let multiplicationTable = Math.multiplicationTable(number)
+
+console.log(multiplicationTable(number));
+
+
+// QUESTION 23
+//
+
